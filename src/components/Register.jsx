4c37ce9b3a2ec38f41343bd4135b9e3d58c79506
2kavitachaubey <chaubeykavita750@gmail.com>
2025-08-07 14:09:19 +0530

    register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,42 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { jsx } from "react/jsx-runtime";
 
 const Register = () => {
-  const [ registerDetail, setRegisterDetail ] = useState({
-    name : "",
-    email : "",
-    phoneNumber: ""
+  const [registerDetail, setRegisterDetail] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
   });
-  const [registeredData, setRegisteredData] = useState([]);
 
-  const handleRegisteredData = (e) =>{
-    e.preventDefault;
-    setRegisteredData((data) => [...data,registerDetail])
-  }
-  console.log(registeredData)
+  const handleRegisteredDetail = (e) => {
+    e.preventDefault();
+    const { name, email, phoneNumber } = registerDetail;    //name : registerDetail.name - kavita
+    if (name && email && phoneNumber) {
+      localStorage.setItem("userDetails", JSON.stringify(registerDetail));
+      setRegisterDetail({
+        name: "",
+        email: "",
+        phoneNumber: "",
+      });
+    } else {
+      return <h2>Please fill all the details</h2>;
+    }
+  };
+
+  useEffect(() => {
+    const data = localStorage.getItem("userDetails");
+    if (data) {
+      setRegisterDetail(JSON.parse(data));
+    }
+  }, []);
+
   return (
     <div>
-      <form onSubmit={handleRegisteredData}>
+      <form onSubmit={handleRegisteredDetail}>
         <input
           type="text"
           placeholder="Full Name"
+          value={registerDetail.name}
           onChange={(e) =>
-            setRegisterDetail((prev) => ({...prev,  name: e.target.value }))
+            setRegisterDetail((prev) => ({ ...prev, name: e.target.value }))
           }
         />
         <input
           type="email"
           placeholder="Email"
+          value={registerDetail.email}
           onChange={(e) =>
-            setRegisterDetail((prev) => ({...prev,  email: e.target.value }))
+            setRegisterDetail((prev) => ({ ...prev, email: e.target.value }))
           }
         />
         <input
           type="tel"
           placeholder="Phone Number"
           pattern="[0-9]{10}"
+          value={registerDetail.phoneNumber}
           onChange={(e) =>
-            setRegisterDetail((prev) => ({...prev,  phoneNumber: e.target.value }))
+            setRegisterDetail((prev) => ({
+              ...prev,
+              phoneNumber: e.target.value,
+            }))
           }
         />
         <button type="submit">Submit</button>
