5ea229c208794cc3ff0a5f3afc1b088d4e668a11
2kavitachaubey <chaubeykavita750@gmail.com>
2025-09-06 00:33:29 +0530

    changes in login page

---
+++ src/components/ArticleHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../pages.css";
+import "../article.css";
+
+const ArticleHeader = () => {
+  return (
+    <div>
+      <div className="article-hero-section">
+        <p className="white-box content">Blogs</p>
+        <h1 className="bg-heading content">
+            Insights & Advice
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default ArticleHeader
