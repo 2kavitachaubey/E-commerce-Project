a9b54096ff1b6a68df61fe0a89ca8c9a52ad5cb1
2kavitachaubey <chaubeykavita750@gmail.com>
2025-09-19 01:50:34 +0530

    made some changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import { CartProvider } from "../lib/cartContext";
 import { FormProvider } from "../lib/formContext";
+import '../aboutUs.css';
+import '../article.css';
+import '../global.css';
 export default function App({ Component, pageProps }) {
   return (
     <FormProvider>
